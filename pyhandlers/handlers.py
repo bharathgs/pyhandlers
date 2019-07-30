a3197f037978e4e5711559f135cc7b5503ee1766
bharath g s <royalkingpin@gmail.com>
2019-07-30 19:26:41 +0530

    deprecation_warnings_handler

--- pyhandlers/handlers.py
+++ pyhandlers/handlers.py
@@ -1,6 +1,7 @@
+import warnings
 from functools import wraps
 
-__all__ = ["error_handler"]
+__all__ = ["error_handler", "deprecation_warnings_handler"]
 
 
 def error_handler(errors=(Exception, ), default=""):
@@ -37,3 +38,15 @@ def error_handler(errors=(Exception, ), default=""):
                 return default(*args, **kwargs) if hasattr(default, '__call__') else default
         return deep_inside
     return funcator
+
+
+def deprecation_warnings_handler(function):
+    '''This is a decorator which can be used to ignore deprecation warnings occurring in a function.'''
+    def new_function(*args, **kwargs):
+        with warnings.catch_warnings():
+            warnings.filterwarnings("ignore", category=DeprecationWarning)
+            return function(*args, **kwargs)
+    new_function.__name__ = function.__name__
+    new_function.__doc__ = function.__doc__
+    new_function.__dict__.update(function.__dict__)
+    return new_function
